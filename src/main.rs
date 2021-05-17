9ab9af7be273f664087962b4e3220fab8bc87d26
rogerpanza <matteo111209@gmail.com>
2021-05-16 21:55:23 -0400

    added config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+// src/main.rs
+pub mod config;
+use config::Config;
+use serenity::{
+  prelude::*,
+  model::prelude::*,
+  Client,
+};
+struct Handler;
+impl EventHandler for Handler {
+  fn message(&self, context: Context, msg: Message) {
+    unimplemented!();
+  }
+}
 fn main() {
-    println!("Hello, world!");
+  let _ = Config::new().save();
+  let config = Config::load().unwrap();
+  let mut client = Client::new(config.token(), Handler)
+    .expect("Couldn't create the new client!");
+  if let Err(why) = client.start() {
+    println!("Client error: {}", why)
+  }
 }
