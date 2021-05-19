af8d21e82c847b392dece4f30e04b94c86d2a39a
rogerpanza <matteo111209@gmail.com>
2021-05-19 17:30:14 -0400

    added help command

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ guild = new Discord.Guild(serverid)
 
 client.once('ready', () => {
     console.log('Ready!');
-    client.user.setActivity('you, I know what you did', { type: 'WATCHING' });
+    client.user.setActivity('egg 2: electric boogaloo', { type: 'WATCHING' });
 });
 
 client.on('message', message => {
@@ -25,6 +25,24 @@ client.on('message', message => {
     if (commandName.guildOnly && message.channel.type === 'dm') {
         return;
     }
+  //easter egg
+  if (commandName === "egg") {
+    message.channel.send("hi im an egg");
+  } //lmao
+
+  if (commandName === 'help') {
+    const hEmbed = new Discord.MessageEmbed()
+        .setColor('#f7ff00')
+        .setTitle('Command list')
+        .setDescription('Prefix is o!')
+        .setTimestamp()
+        .addFields({ name: '**Bot still in development**', value: 'there are no commands for now'})
+        .setFooter('Cassiopeia v0.1 beta');
+    
+    message.channel.send(hEmbed);
+    
+}
+
 }) 
 
 client.login(token);
